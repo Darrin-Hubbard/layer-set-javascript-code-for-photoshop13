23b78af158ac2b727a22953b83bfa2e8449d6020
Darrin Hubbard <123601179+Darrin-Hubbard@users.noreply.github.com>
2024-10-18 21:13:39 -0500

    Update layer_set_object_task.js
    Added missing semicolons to script.

--- layer_set_object_task.js
+++ layer_set_object_task.js
@@ -1,1 +1,12 @@
-// Create a document and an initial layervar docRef = app.documents.add()docRef.artLayers.add()var layerSetRef = docRef.layerSets.add()var layerRef = docRef.artLayers[0].duplicate(layerSetRef,    ElementPlacement.PLACEATEND)    // Link the layers together so that you can move or transform the layers in a single statement.var layerRef1 = docRef.artLayers.add()var layerRef2 = docRef.artLayers.add()layerRef1.link(layerRef2)
+// Create a document and an initial layer
+var docRef = app.documents.add();
+docRef.artLayers.add();
+
+var layerSetRef = docRef.layerSets.add();
+var layerRef = docRef.artLayers[0].duplicate(layerSetRef,
+    ElementPlacement.PLACEATEND);
+    
+// Link the layers together so that you can move or transform the layers in a single statement.
+var layerRef1 = docRef.artLayers.add();
+var layerRef2 = docRef.artLayers.add();
+layerRef1.link(layerRef2);
